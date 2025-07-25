0197f4b015b08099355c798d85e2094f84fd6371
Yuuri Inoue <yuuri041125@gmail.com>
2025-07-25 16:46:39 +0900

    Homeに背景画像を仮で追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import Layout from '@/components/Layout';
+import styles from '@/styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div>
-      <Layout>Home</Layout>
-    </div>
+    <Layout>
+      <div className={styles.hero}>
+        <h1 className="text-white text-2xl text-center font-black">Welcome to My portfolio Page</h1>
+      </div>
+    </Layout>
   );
 }
