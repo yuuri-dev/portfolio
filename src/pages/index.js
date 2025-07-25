2fe58b02237df6fd7aaaf823ce2d0f0402ea2094
Yuuri Inoue <yuuri041125@gmail.com>
2025-07-25 15:51:57 +0900

    index.jsを整理

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,7 @@ import Layout from '@/components/Layout';
 
 export default function Home() {
   return (
-    <div
-      className={`font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
-    >
+    <div>
       <Layout>Home</Layout>
     </div>
   );
