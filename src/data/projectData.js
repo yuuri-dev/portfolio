ba56ed8d0064d9423caca8d4b66aece096fe460f
Yuuri Inoue <yuuri041125@gmail.com>
2025-07-31 23:49:20 +0900

    projectページのモーダルで、詳細な説明とリンクを追加しました。

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,22 +1,41 @@
-// data/projectData.js
 const projectData = [
   {
     id: 1,
     name: 'Shifty',
-    description: 'シフト自動生成アプリ',
+    sub_title: 'シフト自動生成アプリ',
     img: '/images/motibe_timer_top.jpeg',
+    period: '3ヶ月',
+    description: `このアプリは最適なシフトを簡単に出力してくれるアプリケーションです。
+まず、主要な機能としてはシフトの入力、必要人数の入力、シフト生成、作成したシフトの出力です。
+作成したシフトは、pdfや画像として保存することもできます。`,
+    url: 'https://shifty.example.com',
+    stack: ['Next.js', 'Supabase', 'Tailwind CSS'],
+    githubUrl: 'https://github.com/username/repo',
+    liveUrl: 'https://myapp.example.com',
   },
   {
     id: 2,
     name: 'MyBlog',
-    description: 'ハッカソンで作成',
+    sub_title: 'ハッカソンで作成',
     img: '/images/tabree_top.jpeg',
+    period: '2日',
+    description: 'このアプリは....',
+    url: 'https://myblog.example.com',
+    stack: ['Next.js', 'Markdown'],
+    githubUrl: 'https://github.com/username/repo',
+    liveUrl: 'https://myapp.example.com',
   },
   {
     id: 3,
     name: 'ポートフォリオサイト',
-    description: '本サイト',
+    sub_title: '本サイト',
     img: '/images/motibe_timer_top.jpeg',
+    period: '1週間',
+    description: 'このアプリは....',
+    url: 'https://portfolio.example.com',
+    stack: ['React', 'Tailwind CSS'],
+    githubUrl: 'https://github.com/username/repo',
+    liveUrl: 'https://myapp.example.com',
   },
 ];
 
